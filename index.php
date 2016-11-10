aa7c5747c05139faf535ad19ea00ae6e19cc1cae
Adam Mebarki <mebaad10@gmail.com>
2016-11-10 22:45:57 +0000

    azerty

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <html>
-<head>
-    <title>Ma page d'accueil </title>
-</head>
-<body>
-<h1>Bienvenue sur le site de toto </h1>
-<p> Toto fait de l'anglais :</p>
-<?php
-echo '<p>Hello ! What is the day today ? It is '.date("l").' !</p>';
-?>
-</body>
+    <head>
+        <title>Ma page d'accueil </title>
+    </head>
+    <body>
+        <h1>Bienvenue sur le site de toto </h1>
+        <p> Toto fait de l'anglais :</p>
+        <?php
+            $name='testo';
+            $age = '27';
+            $gars = true;
+            $taille = 1.75;
+
+
+            echo '<p>Hello ! What is the day today ? It is '.date("l").' !</p>';
+            echo '<p>Bonjour à tous.</br>
+            Mon vrai nom n\'est pas Toto.</br>
+            Mon vrai nom est '.$name.'</br>
+            J\'ai'.$age.' ans et je mesure'.$taille.'m</br>
+            Et comme mon nom l\'indique, je suis';
+            if ($gars==true){
+                echo 'un garçon.</p>';
+            }
+            else{
+                echo 'une fille.</p>';
+            }
+
+        ?>
+    </body>
 </html>
