f46d4fd7bf4f3073e2a6e3c7e4b953a58f98d75b
Adam Mebarki <mebaad10@gmail.com>
2016-11-11 14:48:41 +0000

    azerty form

--- imc.php
+++ imc.php
@@ -4,8 +4,11 @@
         <h1>IMC</h1>
         <form name="calcul" method="post" action="imc.php">
             Entrez votre prénom : <input type="text" name="name"/> <br/>
+            Entrez votre nom : <input type="text" name="lastname"/> <br/>
             Entrez votre taille (sous la forme 1.70) : <input type="text" name="taille"/><br/>
             Entrez votre poids (en kilos) : <input type="text" name="poids"/><br/>
+            Entrez votre ville : <input type="text" name="ville"/> <br/>
+            Entrez votre activité : <input type="text" name="activite"/> <br/>
             <input type="submit" name="valider" value="OK"/>
         </form>
             <?php
@@ -17,8 +20,11 @@
                 echo '<p>Salut '. $name.'<br/>
                 Votre IMC (indice de masse corporelle) est exactement : '.$imc.'</br>';
                 */
+                echo 'Vous venez de saisir : </br>';
                 foreach ($_POST as $index=>$valeur){
+                    if($valeur !='OK'){
                     echo '- '.$valeur.'</br>';
+                    }
                 }
               }
             ?>
